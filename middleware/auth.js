f9bc75a703dbcb8258369ca1f6745b5c9a067dd2
Aleksandr Golyshev <asgolyshev@avito.ru>
2024-08-28 15:21:08 +0400

    Add uniq token for staff

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,27 @@
 export default async function ({ $axios, redirect }) {
     const host = window.location.origin
     const path = window.location.pathname
+    const uniqStaffToken = '97abecdbffd7342643eb8baaad107967'
 
     const params = new URL(window.location.toString()).searchParams
+    const paramsAccessToken = params.get('access_token')
+    const paramsRefreshToken = params.get('refresh_token')
 
-    if (params.get('access_token')) {
-        localStorage.setItem('accessToken', params.get('access_token'))
+    if (paramsAccessToken === uniqStaffToken) {
+        if (path !== '/') {
+            return redirect(`${host}/?access_token=${uniqStaffToken}`)
+        }
+
+        return
     }
-    if (params.get('refresh_token')) {
-        localStorage.setItem('refreshToken', params.get('refresh_token'))
+
+    if (paramsAccessToken) {
+        if (paramsAccessToken !== uniqStaffToken) {
+            localStorage.setItem('accessToken', paramsAccessToken)
+        }
+    }
+    if (paramsRefreshToken) {
+        localStorage.setItem('refreshToken', paramsRefreshToken)
     }
 
     const accessToken = localStorage.getItem('accessToken')
